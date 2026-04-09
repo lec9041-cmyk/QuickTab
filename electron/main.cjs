6e235138c8c75cc625045ec83c7ea7feb1706d5f
lec9041-cmyk <lec9041@gmail.com>
2026-04-09 15:51:31 +0900

    Set up React runtime and Electron packaging scaffold

---
+++ electron/main.cjs
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const isDev = Boolean(process.env.VITE_DEV_SERVER_URL);
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+    return;
+  }
+
+  mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
